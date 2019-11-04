e19adae3a96b1b9cfd2e72214f160ad9624c2a88
MOAAS <miguel.moas@gmail.com>
2019-11-04 11:42:01 +0000

    double efficiency

--- lmaoooooo.pl
+++ lmaoooooo.pl
@@ -18,18 +18,33 @@ disp:-
     display_game(X, 1).
 
 display_game(Board, Player):-
-    disp_game_first(Board, 1 , 1), %disp hex board
-    disp_game_last_row(Board),     %completes missing part of last hex row
+    display_board(Board),
     disp_player_turn(Player).      %prints player turn info
     
-
-
-disp_player_turn(Player):- %displays player turn 
+/*
+Displays player turn
+Arguments:
+- player
+*/
+disp_player_turn(Player):- 
     write('Player '), write(Player), write(' turn ').
 
-disp_game([], _OddRow , _). % base case -> when there are no more rows,disp_game finishes here
+/*
+Displays board
+Arguments:
+- board
+*/
+display_board([Row | Board]):-
+    disp_board_first(Row),            %disp hex board
+    disp_board_general(Board, 0 , 2),      %starts from 2nd row
+    disp_board_last(Board).         %completes missing part of last hex row
 
-disp_game_first([Row | Board], 1 , X):- %the first row is different from the rest 
+/*
+Displays board's first row
+Arguments:
+- row
+*/
+disp_board_first(Row):- %the first row is different from the rest 
     write('    '),
     displayRow1(Row),
     nl,
@@ -44,14 +59,20 @@ disp_game_first([Row | Board], 1 , X):- %the first row is different from the res
     nl,
 
     write('    '),
-    displayRow4(Row,X,1),
+    displayRow4(Row,1,1),
     write('|'),
-    nl,
-    Xnew is X+1,         %increment X
-    disp_game(Board, 0 , Xnew). %after this (diferent) row, all the others will be made recursively the same way
+    nl.
 
+/*
+Displays board
+Arguments:
+- board to be displayed (list of lists)
+- 1 if odd row, 0 otherwise
+- row number
+*/
+disp_board_general([], _OddRow , _). % base case -> when there are no more rows,disp_game finishes here
 
-disp_game([Row | Board], 1 , X):- %odd numbered row print. Each row needs to print 4 lines to make hexagon patterns
+disp_board_general([Row | Board], 1 , X):- %odd numbered row print. Each row needs to print 4 lines to make hexagon patterns
     write(' \\  '),    %first print line
     displayRow1(Row),  %odd numbered rows need to start ahead so they can fit in the pattern
     nl,
@@ -65,17 +86,17 @@ disp_game([Row | Board], 1 , X):- %odd numbered row print. Each row needs to pri
     write('|'),
     nl,
 
-    write('    '),     %forth print line
-    displayRow4(Row,X,1),
+    write('    '),     %fourth print line
+    displayRow4(Row, X, 1),
     write('|'),
     nl,
 
-    Xnew is X+1,         %increment X
+    X1 is X+1,         %increment X
 
-    disp_game(Board, 0 ,Xnew).%moving over to the next row, alternating between even and odd.
+    disp_board_general(Board, 0, X1).%moving over to the next row, alternating between even and odd.
 
 
-disp_game([Row | Board], 0,X):- %even numbered row print
+disp_board_general([Row | Board], 0, X):- %even numbered row print
     displayRow1(Row),%first print line
     write('   /'),
     nl,
@@ -88,24 +109,37 @@ disp_game([Row | Board], 0,X):- %even numbered row print
     write('|'),
     nl,
 
-    displayRow4(Row,X,1),%forth print line
+    displayRow4(Row, X, 1),%fourth print line
     write('|'),
     nl,
-    Xnew is X+1,         %increment X
-    disp_game(Board, 1 ,Xnew).
+    X1 is X+1,         %increment X
+    disp_board_general(Board, 1, X1).
 
+/*
+Displays the first print line of a board row
+Arguments:
+- Row to be displayed
+*/
 displayRow1([]).
-
 displayRow1([_ | Row]):-
     write('   / \\  '),
     displayRow1(Row).
 
+/*
+Displays the second print line of a board row
+Arguments:
+- Row to be displayed
+*/
 displayRow2([]).
-
 displayRow2([_ | Row]):-
     write(' /     \\'),
     displayRow2(Row).
 
+/*
+Displays the third print line of a board row
+Arguments:
+- Row to be displayed
+*/
 displayRow3([]). 
 
 displayRow3([0 | Row]):-
@@ -124,32 +158,45 @@ displayRow3([3 | Row]):-
     write('|  OOO  '),
     displayRow3(Row).
 
-
+/*
+Displays the fourth print line of a board row
+Arguments:
+- Row to be displayed
+*/
 displayRow4([],_,_). 
 
-displayRow4([ A | Row],X,Y):- 
-    A = 0, %forth line only prints coordinates if there are pieces in that hex
-    !,
+displayRow4([ 0 | Row], X, Y):-  %fourth line only prints coordinates if there are pieces in that hex    
     write('|       '), %if there are no pieces, it doesnt show the coords
-    Ynew is Y+1,    %increment Y 
-    displayRow4(Row, X , Ynew).
+    Y1 is Y+1, %increment Y 
+    displayRow4(Row, X , Y1).
 
 displayRow4([ _A | Row],X,Y):- %else , prints the coords
     write('| '),
-    displayDynSize(X), %print X
+    displayCoord(X), %print X
     write(','),
-    displayDynSize(Y), %print Y
+    displayCoord(Y), %print Y
     write(' '), 
-    Ynew is Y+1,        %increment Y 
-    displayRow4(Row, X , Ynew). 
+    Y1 is Y+1,        %increment Y 
+    displayRow4(Row, X , Y1). 
 
-displayDynSize(V):- %if the number has 2 digits doesnt print the space so it can fit in the hex
-    V > 9 , ! , write(V).
-displayDynSize(V):-
+
+/**
+Dynamic single coordinate printing
+Arguments:
+- Number of coordinate to print
+*/
+displayCoord(V):- %if the number has 2 digits doesnt print the space so it can fit in the hex
+    V > 9 , write(V).
+displayCoord(V):-
     write(V),write(' ').
 
 
-disp_game_last_row([[_Row | SmallerRow] | _Board]):- %prints the missing characters to complete the last row hexagon pattern
+/*
+Completes last row 
+Arguments:
+- Board
+*/
+disp_board_last([[_Row | SmallerRow] | _Board]):- %prints the missing characters to complete the last row hexagon pattern
     write('     \\  '),         %first print line
     displayRow1(SmallerRow),    %prints rowsize - 1 iterations 
     write('   /'),
