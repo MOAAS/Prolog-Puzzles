e471b449b74ddd1f498cd1f826b8476df8f5d5e4
MOAAS <miguel.moas@gmail.com>
2019-12-17 21:18:07 +0000

    + puzzle 20

--- TP2/src.pl
+++ TP2/src.pl
@@ -42,7 +42,7 @@ solvePuzzle(Puzzle, Solution):-
     labeling([], Solution).
 
 solver:-
-    puzzle8(Puzzle),
+    puzzle20(Puzzle),
     solvePuzzle(Puzzle, Solution),
     write(Solution).
 
@@ -76,5 +76,42 @@ puzzle8([
 ]).
 
 
-
+puzzle20([
+    branch(-4, [
+        weight(-3,_),
+        weight(-2,_),
+        weight(3,_)
+    ]),
+    branch(-2, [
+        weight(-2,_),
+        weight(-1,_),
+        branch(1, [
+            branch(-1, [
+                weight(-1,_),
+                weight(2,_),
+                weight(3,_)
+            ]),
+            weight(1,_),
+            weight(2,_)
+        ])
+    ]),
+    weight(1,_),
+    branch(3, [
+        branch(-1, [
+            branch(-1, [
+                weight(-1,_),
+                branch(1, [
+                    weight(-2,_),
+                    weight(1,6)
+                ]),
+                weight(2,_),
+                weight(3,_)
+            ]),
+            weight(1,_),
+            weight(2,_)
+        ]),
+        weight(1,_),
+        weight(2,_)
+    ])
+]).
 
